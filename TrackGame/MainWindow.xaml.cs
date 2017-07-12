d0cc1cbc267e39a7ee1024587510b5c7e21974ad
zmidl <zmidl@hotmail.com>
2017-07-12 13:11:59 +0800

    20170712

--- TrackGame/MainWindow.xaml.cs
+++ TrackGame/MainWindow.xaml.cs
@@ -24,28 +24,20 @@ namespace TrackGame
 		private Button button = new Button();
 
 		private readonly int 地块数量 = 35;
-		private readonly int 地块尺寸 = 50;
+		private readonly int 地块尺寸 = 100;
 		private readonly Point 起点坐标 = new Point(1, 2);
 		private List<地块> 地块列队;
-		private List<地块> 已铺地块列队 = new List<地块>(1);
+		private List<地块> 已铺地块列队 = new List<地块>(35);
+		private List<Path> 路径集合 = new List<Path>();
 
-		PathSegmentCollection segmentCollection;
-		Path path;
-		PathGeometry pathGeometry;
 		private void 初始化路径(Point startPoint)
 		{
-			this.path = new Path();
-			pathGeometry = new PathGeometry();
-			PathFigure pathFigure = new PathFigure();
-			pathFigure.StartPoint = new Point((startPoint.X * this.地块尺寸) + (this.地块尺寸 * 0.5), (startPoint.Y * this.地块尺寸) + (this.地块尺寸 * 0.5));
-			segmentCollection = new PathSegmentCollection();
-			segmentCollection.Add(new LineSegment() { Point = new Point((startPoint.X * this.地块尺寸) + this.地块尺寸, (startPoint.Y * this.地块尺寸) + (this.地块尺寸 * 0.5)) });
-			pathFigure.Segments = segmentCollection;
-			pathGeometry.Figures = new PathFigureCollection() { pathFigure };
-			this.path.Data = pathGeometry;
-			this.path.Stroke = new SolidColorBrush(Colors.Red);
-			this.path.StrokeThickness = 3;
-			this.Grid_Ground.Children.Add(this.path);
+
+			var sp = new Point((startPoint.X * this.地块尺寸) + (this.地块尺寸 * 0.5), (startPoint.Y * this.地块尺寸) + (this.地块尺寸 * 0.5));
+			var ep = new Point((startPoint.X * this.地块尺寸) + this.地块尺寸, (startPoint.Y * this.地块尺寸) + (this.地块尺寸 * 0.5));
+			var 路径 = this.计算路径(sp, ep);
+			this.路径集合.Add(路径);
+			this.Grid_Ground.Children.Add(路径);
 		}
 
 		public MainWindow()
@@ -56,7 +48,7 @@ namespace TrackGame
 
 			button.Width = 80;
 			button.Height = 30;
-			
+
 			this.Canvas_Ground.Children.Add(button);
 		}
 
@@ -70,7 +62,7 @@ namespace TrackGame
 					Name = $"N_{i % 7}_{i / 7}",
 					Width = this.地块尺寸,
 					Height = this.地块尺寸,
-					Margin = new Thickness((i % 7) * 50, (i / 7) * 50, 0, 0),
+					Margin = new Thickness((i % 7) * this.地块尺寸, (i / 7) * this.地块尺寸, 0, 0),
 					IsEnabled = false
 				};
 				地块对象.Click += 铺设铁轨;
@@ -85,10 +77,14 @@ namespace TrackGame
 		private void 铺设铁轨(object sender, RoutedEventArgs e)
 		{
 			
+
+			// 由于重新设置地块 所以前一次计算的“下一地块”需要重置
 			if (this.下一地块缓存 != null) this.下一地块缓存.IsEnabled = false;
+
+			// 得到当前设置的地块对象
 			地块 当前铺设的地块 = (地块)sender;
 
-			// 点击的地块不是老地方的话
+			// 点击的地块不是老地方的话也就是说往下个地块点击
 			if (this.已铺地块列队.FirstOrDefault(o => o == 当前铺设的地块) == null)
 			{
 				this.已铺地块列队.Add(当前铺设的地块);
@@ -96,9 +92,13 @@ namespace TrackGame
 			}
 			else
 			{
-				if(当前铺设的地块.路线!=null) this.segmentCollection.RemoveAt(this.segmentCollection.Count - 1);
+				if (当前铺设的地块.路线 != null)
+				{
+					this.路径集合.RemoveAt(this.路径集合.Count - 1);
+					this.Canvas_Ground.Children.RemoveAt(this.Canvas_Ground.Children.Count - 1);
+				}
 			}
-
+			
 			// 需要上一段铁轨的信息 来确定当前铁轨的路径
 			当前铺设的地块.随机变化地块(this.已铺地块列队[this.已铺地块列队.Count - 2]);
 			地块 下一地块 = this.获取下一地块(当前铺设的地块);
@@ -108,9 +108,15 @@ namespace TrackGame
 			if (下一地块 != null) 下一地块.IsEnabled = true;//this.Title = "成功";
 
 			var 当前路径 = this.画节点路径(当前铺设的地块);
-			if(当前路径!=null) this.segmentCollection.Add(当前路径);
+			if (当前路径 != null)
+			{
+				this.路径集合.Add(当前路径);
+				this.Canvas_Ground.Children.Add(当前路径);
+			}
+			//else this.Canvas_Ground.Children.Remove(当前路径);
 
-			//this.Title = this.已铺地块列队.Count.ToString();
+
+			//this.Title = this.路径集合.Count.ToString();
 		}
 
 		private void 设置起点地块(Point 起点坐标)
@@ -155,35 +161,38 @@ namespace TrackGame
 			return 下一地块;
 		}
 
-		private PathSegment 画节点路径(地块 当前地块)
+		private Path 画节点路径(地块 当前地块)
 		{
-			var result = default(PathSegment);
-			if(当前地块.路线!=null)
+			var result = default(Path);
+			if (当前地块.路线 != null)
 			{
-				var 节点坐标 = new Point((当前地块.坐标.X + 当前地块.路线.终点坐标.X) * 当前地块.Width, (当前地块.坐标.Y + 当前地块.路线.终点坐标.Y) * 当前地块.Height);
+				var 节点起点坐标 = new Point((当前地块.坐标.X + 当前地块.路线.起点坐标.X) * 当前地块.Width, (当前地块.坐标.Y + 当前地块.路线.起点坐标.Y) * 当前地块.Height);
+
+				var 节点终点坐标 = new Point((当前地块.坐标.X + 当前地块.路线.终点坐标.X) * 当前地块.Width, (当前地块.坐标.Y + 当前地块.路线.终点坐标.Y) * 当前地块.Height);
 
 				if (当前地块.路线.起点方向 == 方向.右 && 当前地块.路线.终点方向 == 方向.上 ||
 					当前地块.路线.起点方向 == 方向.下 && 当前地块.路线.终点方向 == 方向.右 ||
 					当前地块.路线.起点方向 == 方向.左 && 当前地块.路线.终点方向 == 方向.下 ||
 					当前地块.路线.起点方向 == 方向.上 && 当前地块.路线.终点方向 == 方向.左)
 				{
-					result = new ArcSegment(节点坐标, new Size(当前地块.Width / 2, 当前地块.Height / 2), 90, false, SweepDirection.Clockwise, true);
+					result = this.计算路径(节点起点坐标, 节点终点坐标, SweepDirection.Clockwise); //new ArcSegment(节点坐标, new Size(当前地块.Width / 2, 当前地块.Height / 2), 90, false, SweepDirection.Clockwise, true);
 				}
 				else if (当前地块.路线.起点方向 == 方向.左 && 当前地块.路线.终点方向 == 方向.上 ||
 					当前地块.路线.起点方向 == 方向.下 && 当前地块.路线.终点方向 == 方向.左 ||
 					当前地块.路线.起点方向 == 方向.右 && 当前地块.路线.终点方向 == 方向.下 ||
 					当前地块.路线.起点方向 == 方向.上 && 当前地块.路线.终点方向 == 方向.右)
 				{
-					result = new ArcSegment(节点坐标, new Size(当前地块.Width / 2, 当前地块.Height / 2), 90, false, SweepDirection.Counterclockwise, true);
+					result = this.计算路径(节点起点坐标, 节点终点坐标, SweepDirection.Counterclockwise);
 				}
-				else result = new LineSegment() { Point = 节点坐标 };
+				else result = this.计算路径(节点起点坐标, 节点终点坐标);
 			}
 			return result;
 		}
 
 
-		private void 开火车(FrameworkElement element, Path path, double speed)
+		private void 开火车(FrameworkElement element, List<Path> paths, double speed)
 		{
+			var path = paths[0];
 			element.RenderTransformOrigin = new Point(0.5, 0.5);
 
 			TranslateTransform translate = new TranslateTransform();
@@ -224,37 +233,55 @@ namespace TrackGame
 			Storyboard.SetTargetProperty(animationX, new PropertyPath(TranslateTransform.XProperty));
 			Storyboard.SetTargetProperty(animationY, new PropertyPath(TranslateTransform.YProperty));
 			Storyboard.SetTargetProperty(animationAngle, new PropertyPath(RotateTransform.AngleProperty));
-			story.Completed += (s, e) => { MessageBox.Show(""); };
+			story.Completed += (s, e) => 
+			{
+				if (paths.Count > 1)
+				{
+					paths.RemoveAt(0);
+					this.开火车(element, paths, speed);
+				}
+				else
+				{
+					MessageBox.Show("Stop");
+				}
+			};
 			story.Begin(this);
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			Path path = new Path();
-			PathGeometry pathGeometry = new PathGeometry();
-			ArcSegment arc = new ArcSegment(new Point(100, 200), new Size(50, 100), 0, false, SweepDirection.Counterclockwise, true);
-			PathFigure figure = new PathFigure();
-			figure.StartPoint = new Point(100, 0);
-			figure.Segments.Add(arc);
-			pathGeometry.Figures.Add(figure);
-			path.Data = pathGeometry;
-			path.Stroke = Brushes.Red;
-			path.StrokeThickness = 4;
-			this.Canvas_Ground.Children.Add(path);
+			this.开火车(button, this.路径集合, 1);
+		}
 
-			
-
-
-			LineGeometry myLineGeometry = new LineGeometry();
-			myLineGeometry.StartPoint = new Point(200,50);
-			myLineGeometry.EndPoint = new Point(100, 90);
-
-			Path myPath = new Path();
-			myPath.Stroke = Brushes.Red;
-			myPath.StrokeThickness = 4;
-			myPath.Data = myLineGeometry;
-
-			this.Canvas_Ground.Children.Add(myPath);
+		private Path 计算路径(Point 起点, Point 终点, SweepDirection? 方向 = null)
+		{
+			var 结果 = new Path();
+			结果.Stroke = Brushes.Red;
+			结果.StrokeThickness = 4;
+			switch (方向)
+			{
+				case SweepDirection.Clockwise:
+				case SweepDirection.Counterclockwise:
+				{
+					PathGeometry pathGeometry = new PathGeometry();
+					ArcSegment 圆弧 = new ArcSegment(new Point(终点.X, 终点.Y), new Size(this.地块尺寸 / 2, this.地块尺寸 / 2), 0, false, (SweepDirection)方向, true);
+					PathFigure figure = new PathFigure();
+					figure.StartPoint = new Point(起点.X, 起点.Y);
+					figure.Segments.Add(圆弧);
+					pathGeometry.Figures.Add(figure);
+					结果.Data = pathGeometry;
+					break;
+				}
+				default:
+				{
+					LineGeometry 直线 = new LineGeometry();
+					直线.StartPoint = new Point(起点.X, 起点.Y);
+					直线.EndPoint = new Point(终点.X, 终点.Y);
+					结果.Data = 直线;
+					break;
+				}
+			}
+			return 结果;
 		}
 	}
 }
